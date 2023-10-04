bab20d6029a90723b87a57a02de3cc159ec2d95c
Jonathan0008 <s22110362@student.unklab.ac.id>
2023-10-04 10:23:44 +0700

    up

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ console.log("ngana pe nama");
 console.log("lapar ehhh");
 console.log("bowlehhh");
 console.log("makang apa kang?");
-<<<<<<< HEAD
 console.log("anjeli kata")
 console.log("hallo dg")
-=======
 console.log("Yang mo beking/service kacamata hubungi ini ; 089626368687")
 console.log("anjeli kata")
->>>>>>> 04c07c1a407f8d47b56c3bde85f69c59f5009efc
