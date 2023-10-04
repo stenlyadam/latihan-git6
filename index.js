eadba59811343593815f2a4b06df31177a48c83f
Slyxes5 <avantakun@gmail.com>
2023-10-04 11:27:33 +0800

    name

--- index.js
+++ index.js
@@ -8,14 +8,8 @@ console.log("ngana pe nama");
 console.log("lapar ehhh");
 console.log("bowlehhh");
 console.log("makang apa kang?");
-<<<<<<< HEAD
 console.log("Politon, Vrandly Thedore");
-=======
-<<<<<<< HEAD
 console.log("anjeli kata")
 console.log("hallo dg")
-=======
 console.log("Yang mo beking/service kacamata hubungi ini ; 089626368687")
 console.log("anjeli kata")
->>>>>>> 04c07c1a407f8d47b56c3bde85f69c59f5009efc
->>>>>>> f4ee72c8511e996ff244867ea9aeef788ed91d94
