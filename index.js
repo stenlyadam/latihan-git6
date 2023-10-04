464269ae23af04f36925c45e7a34504009af86fb
nathanlukass <s22110408@student.unklab.ac.id>
2023-10-04 11:09:01 +0800

    makangg

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ console.log("tambah kode hari senin");
 console.log("Stenly Adam");
 console.log("Hello World");
 console.log("ngana pe nama");
-console.log("lapar ehhh")
+console.log("lapar ehhh");
+console.log("bowlehhh");
+console.log("makang apa kang?");
